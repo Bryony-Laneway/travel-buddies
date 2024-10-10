412f92ccc194e0405cd73ec4dc803b4d7d978476
BryonyKim <82268148+BryonyKim@users.noreply.github.com>
2024-10-10 17:13:54 +1100

    seed remove password

--- back-end/seed.js
+++ back-end/seed.js
@@ -1,33 +1,33 @@
 // seed.js
-const mysql = require('mysql2');
-const fs = require('fs');
+const mysql = require("mysql2");
+const fs = require("fs");
 
 // Read the seed SQL file
-const seedSql = fs.readFileSync('seed.sql', 'utf8');
+const seedSql = fs.readFileSync("seed.sql", "utf8");
 
 // Create a connection to the MySQL database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Qwert@123',
-  database: 'travel_buddies',
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "travel_buddies",
   multipleStatements: true,
 });
 
 // Function to seed the database
 const seedDatabase = () => {
-  connection.connect(err => {
+  connection.connect((err) => {
     if (err) {
-      console.error('Error connecting to MySQL:', err);
+      console.error("Error connecting to MySQL:", err);
       return;
     }
-    console.log('Connected to MySQL.');
+    console.log("Connected to MySQL.");
 
     connection.query(seedSql, (err, results) => {
       if (err) {
-        console.error('Error executing seed SQL:', err);
+        console.error("Error executing seed SQL:", err);
       } else {
-        console.log('Database seeded successfully.');
+        console.log("Database seeded successfully.");
       }
       connection.end();
     });
